cde919915db7d0d481f5cb54e944eec2c86f3bd1
Your GitHub Username <anugrahalifiandi20@gmail.com>
2024-07-04 15:35:01 +0800

    useFonts

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react' 
-import { Colors } from 'react-native/Libraries/NewAppScreen'
-const App = () => {
+import { useFonts} from 'expo-font';
+import { Text, View } from 'react-native';//usefonts
+export default function App() {
+  const [dapatFont] = useFonts({
+    'MetroBlack' : require('./assets/fonts/Metropolis-Black.otf'),
+    'MetroBold' : require('./assets/fonts/Metropolis-Bold.otf'),
+    'MetroLight' : require('./assets/fonts/Metropolis-Light.otf'),
+    'MetroMedium' : require('./assets/fonts/Metropolis-Medium.otf'),
+    'MetroSemiBold' : require('./assets/fonts/Metropolis-SemiBold.otf'),
+  });
+  if(!dapatFont) return <Text>Font Tidak ditemukan ...</Text>
   return (
-  <View style={{flex:1}}>
-    <View style={{ flex: 1, flexDirection: "row" }}>
-      <View style={{ flex: 1, backgroundColor: 'green' }}></View>
-      <View style={{ flex: 1, backgroundColor: 'yellow' }}></View>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{ fontFamily: 'MetroBlack'}}>Font Metro Black</Text>
+      <Text style={{ fontFamily: 'MetroBold'}}>Font Metro Bold</Text>
+      <Text style={{ fontFamily: 'MetroLight'}}>Font Metro Light</Text>
+      <Text style={{ fontFamily: 'MetroMedium'}}>Font Metro Medium</Text>
+      <Text style={{ fontFamily: 'MetroSemiBold'}}>Font Metro SemiBold</Text>
     </View>
-    <View style={{ flex: 1, flexDirection: "column" }}>
-      <View style={{ flex: 1, backgroundColor: 'red' }}></View>
-      <View style={{ flex: 1, backgroundColor: 'blue' }}></View>
-    </View>
-    </View>
-
-  )
+  );
 }
-export default App
